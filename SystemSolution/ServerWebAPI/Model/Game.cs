9d317985689901704c936a5ef64faccb65a63e74
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-04-01 10:46:22 +0200

    Created Model Game and User, and DbContext

---
+++ SystemSolution/ServerWebAPI/Model/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServerWebAPI.Model
+{
+	public class Game
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string DeveloperTeam { get; set; }
+	}
+}
