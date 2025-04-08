460faeec02761a7b7f4157463e69f68f4ade71c2
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-04-08 09:54:51 +0200

    feat(api): Modificat Program.cs

--- SystemSolution/ServerWebAPI/Program.cs
+++ SystemSolution/ServerWebAPI/Program.cs
@@ -1,14 +1,20 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using ServerWebAPI.Data;
 using ServerWebAPI.Model;
 using System;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 namespace ServerWebAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -16,24 +22,102 @@ namespace ServerWebAPI
 			var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 			builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 
+			builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+			{
+				options.Password.RequireDigit = true;
+				options.Password.RequiredLength = 6;
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequireUppercase = true;
+				options.Password.RequireLowercase = true;
+				options.User.RequireUniqueEmail = true;
+				options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+				options.Lockout.MaxFailedAccessAttempts = 5;
+				options.Lockout.AllowedForNewUsers = true;
+				options.SignIn.RequireConfirmedEmail = false;
 
+			})
+				.AddEntityFrameworkStores<AppDbContext>()
+				.AddDefaultTokenProviders();
+
+			var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+			builder.Services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+				.AddJwtBearer(options =>
+				{
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						ValidateIssuer = true,
+						ValidIssuer = jwtSettings["Issuer"],
+						ValidateAudience = true,
+						ValidAudience = jwtSettings["Audience"],
+						ValidateLifetime = true,
+						ClockSkew = TimeSpan.Zero,
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]))
+					};
+				});
+
+			builder.Services.AddAuthorization();
 			// Add services to the container.
 
-			builder.Services.AddControllers();
-
-            var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-
-			if (app.Environment.IsDevelopment())
+			builder.Services.AddControllers().AddJsonOptions(options =>
 			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
+				options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+			}); ;
+
+			builder.Services.AddOpenApi();
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen(opt =>
+			{
+				opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					In = ParameterLocation.Header,
+					Description = "Please enter token",
+					Name = "Authorization",
+					Type = SecuritySchemeType.Http,
+					BearerFormat = "JWT",
+					Scheme = "bearer"
+				});
+				opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme
+						{
+							Reference = new OpenApiReference
+							{
+								Type = ReferenceType.SecurityScheme,
+								Id = "Bearer"
+							}
+						},
+						new string[] {}
+					}
+				});
+			});
+
+			var app = builder.Build();
+
+			using (var scope = app.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+				await Tools.RoleTools.CreateRolesInitials(services);
 			}
+
+				// Configure the HTTP request pipeline.
+
+				if (app.Environment.IsDevelopment())
+				{
+					app.MapOpenApi();
+					app.UseSwagger();
+					app.UseSwaggerUI();
+				}
 
 			app.UseHttpsRedirection();
 
-            app.UseAuthorization();
+			app.UseAuthentication();
+			app.UseAuthorization();
 
 
             app.MapControllers();
