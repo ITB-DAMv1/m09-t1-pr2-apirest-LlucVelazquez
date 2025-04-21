aa070206c2ef7717117fd2544a7c2d398ce71505
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-04-21 19:09:38 +0200

    feat(client): add/edit/delete game

--- SystemSolution/ClientWebRP/Tools/TokenHelper.cs
+++ SystemSolution/ClientWebRP/Tools/TokenHelper.cs
@@ -2,25 +2,25 @@
 
 namespace ClientWebRP.Tools
 {
-	public class TokenHelper
-	{
-		public static bool IsTokenSession(string token)
-		{
-			return !string.IsNullOrEmpty(token) && !IsTokenExpired(token);
-		}
+    public class TokenHelper
+    {
+        public static bool IsTokenSession(string token)
+        {
+            return !string.IsNullOrEmpty(token) && !IsTokenExpired(token);
+        }
 
-		public static bool IsTokenExpired(string token)
-		{
-			var handler = new JwtSecurityTokenHandler();
-			if (string.IsNullOrWhiteSpace(token) || !handler.CanReadToken(token))
-			{
-				return true; // Treat as expired if token is invalid or not readable
-			}
+        public static bool IsTokenExpired(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            if (string.IsNullOrWhiteSpace(token) || !handler.CanReadToken(token))
+            {
+                return true; // Treat as expired if token is invalid or not readable
+            }
 
-			var jwt = handler.ReadJwtToken(token);
-			var expiration = jwt.ValidTo;
-			return expiration < DateTime.UtcNow;
-		}
+            var jwt = handler.ReadJwtToken(token);
+            var expiration = jwt.ValidTo;
+            return expiration < DateTime.UtcNow;
+        }
         public static string GetTokenExpiration(string token)
         {
             var handler = new JwtSecurityTokenHandler();
@@ -32,9 +32,9 @@ namespace ClientWebRP.Tools
             var expiration = jwt.ValidTo;
             return expiration.ToString("yyyy-MM-dd HH:mm:ss");
         }
-		public static bool IsTokenAdmin(string token)
-		{
-			var handler = new JwtSecurityTokenHandler();
+        public static bool IsTokenAdmin(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
             if (string.IsNullOrWhiteSpace(token) || !handler.CanReadToken(token))
             {
                 return false; // Treat as not admin if token is invalid or not readable
@@ -54,4 +54,5 @@ namespace ClientWebRP.Tools
             var nameClaim = jwt.Claims.FirstOrDefault(c => c.Type == "name");
             return nameClaim?.Value ?? "No name claim found";
         }
+    }
 }
