8e5e266594e2ff01d3277d5bcc90f32f37d77830
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-04-21 12:04:12 +0200

    feat(api): Part del Servidor Xat

---
+++ SystemSolution/ServerWebAPI/Hubs/XatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ServerWebAPI.Hubs
+{
+    public class XatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
--- SystemSolution/ServerWebAPI/Program.cs
+++ SystemSolution/ServerWebAPI/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using ServerWebAPI.Data;
+using ServerWebAPI.Hubs;
 using ServerWebAPI.Model;
 using System;
 using System.Text;
@@ -96,8 +97,9 @@ namespace ServerWebAPI
 					}
 				});
 			});
+            builder.Services.AddSignalR();
 
-			var app = builder.Build();
+            var app = builder.Build();
 
 			using (var scope = app.Services.CreateScope())
 			{
@@ -121,7 +123,7 @@ namespace ServerWebAPI
 
 
             app.MapControllers();
-
+			app.MapHub<XatHub>("/UserXat");
             app.Run();
         }
     }
