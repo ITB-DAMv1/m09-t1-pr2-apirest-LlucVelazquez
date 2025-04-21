905927c8d93b57d07cab2e0dad4b9dae384dc0a5
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-04-21 14:47:29 +0200

    Half fixed signalR

--- SystemSolution/ClientWebRP/wwwroot/js/chat.js
+++
@@ -1,7 +0,0 @@
-﻿const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/xatHub")
-    .build();
-
-connection.start()
-    .then(() => console.log("Conexión establecida"))
-    .catch(err => console.error("Error al conectar:", err));
