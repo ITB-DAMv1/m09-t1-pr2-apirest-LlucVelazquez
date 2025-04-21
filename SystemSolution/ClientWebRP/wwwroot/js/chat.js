d4afd0f2eb99f8b7ff80842628bbfbf26a803bd5
Lluc Velazquez <lluc.velazquez.7e8@itb.cat>
2025-04-21 14:10:14 +0200

    fixed signalr

---
+++ SystemSolution/ClientWebRP/wwwroot/js/chat.js
@@ -0,0 +1,7 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/xatHub")
+    .build();
+
+connection.start()
+    .then(() => console.log("Conexión establecida"))
+    .catch(err => console.error("Error al conectar:", err));
--- SystemSolution/ClientWebRP/wwwroot/js/site.js
+++ SystemSolution/ClientWebRP/wwwroot/js/site.js
@@ -2,10 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/xatHub")
-    .build();
-
-connection.start()
-    .then(() => console.log("Conexión establecida"))
-    .catch(err => console.error("Error al conectar:", err));
